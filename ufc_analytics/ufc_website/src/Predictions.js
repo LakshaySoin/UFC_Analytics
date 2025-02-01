a6a1b43a5cb2428956a77b8d900b45cb1ed20ebe
Lakshay Soin <lakshaysoin@gmail.com>
2025-02-01 13:00:14 -0800

    updated results for fight night: adesanya vs imavov

--- ufc_analytics/ufc_website/src/Predictions.js
+++ ufc_analytics/ufc_website/src/Predictions.js
@@ -6,11 +6,12 @@ function Predictions() {
   const ufc_311_winners = [1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1];
   const ufc_311_odds = [1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1];
   const ufc_fight_night_1_odds = [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0];
+  const ufc_fight_night_1_winners = [0, 0, 1, 0, -1, 1, 1, 1, 1, 1, 1, 1, -1];
 
   return (
     <div>
       <h1 className='head'>Upcoming Event</h1>
-      <Event title={"UFC Fight Night: Adesanya vs. Imavov"} event={"FightNight1"} odds={ufc_fight_night_1_odds} result={null} />
+      <Event title={"UFC Fight Night: Adesanya vs. Imavov"} event={"FightNight1"} odds={ufc_fight_night_1_odds} result={ufc_fight_night_1_winners} />
       <h1 className='head'>Past Events</h1>
       <Event title={"UFC 311: Makhachev vs. Moicano"} event={"UFC311"} odds={ufc_311_odds} result={ufc_311_winners} />
     </div>
