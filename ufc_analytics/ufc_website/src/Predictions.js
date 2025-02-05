562f3efa1ddae0a81f10020f417dc8bb26ea9623
Lakshay Soin <lakshaysoin@gmail.com>
2025-02-05 10:41:49 -0800

    updated for du plessis vs strickland 2

--- ufc_analytics/ufc_website/src/Predictions.js
+++ ufc_analytics/ufc_website/src/Predictions.js
@@ -7,12 +7,14 @@ function Predictions() {
   const ufc_311_odds = [1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1];
   const ufc_fight_night_1_odds = [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0];
   const ufc_fight_night_1_winners = [0, 0, 1, 0, -1, 1, 1, 1, 1, 1, 1, 1, -1];
+  const ufc_312_odds = [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1]
 
   return (
     <div>
       <h1 className='head'>Upcoming Event</h1>
-      <Event title={"UFC Fight Night: Adesanya vs. Imavov"} event={"FightNight1"} odds={ufc_fight_night_1_odds} result={ufc_fight_night_1_winners} />
+      <Event title={"UFC 312: Du Plessis vs Strickland 2"} event="UFC312" odds={ufc_312_odds} result={null} />
       <h1 className='head'>Past Events</h1>
+      <Event title={"UFC Fight Night: Adesanya vs. Imavov"} event={"FightNight1"} odds={ufc_fight_night_1_odds} result={ufc_fight_night_1_winners} />
       <Event title={"UFC 311: Makhachev vs. Moicano"} event={"UFC311"} odds={ufc_311_odds} result={ufc_311_winners} />
     </div>
   )
